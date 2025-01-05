654a3d86ff074c407201ea1f128a8276e3e5d61a
Anil Kaiyambally Sudhakar <anils.kaiyamballi@gmail.com>
2025-01-05 20:06:41 +0100

    added conan setup

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ CLEAR='\033[0m'
 
 BUILD_DIR="_build-output"
 
+conan profile detect
+
+conan remote add simple-conan https://anilkaiy.jfrog.io/artifactory/api/conan/simple-conan
+
 conan install . --output-folder=$BUILD_DIR --build=missing
 if [ $? -ne 0 ]; then
     echo -e "${RED}Conan failed to install dependencies. Check logs for additional information${CLEAR}"
